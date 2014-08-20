11eec0d4db82409801a798cbfb9b077f04e51bee
Heitor C <hcproradio@gmail.com>
2014-08-19 21:42:27 -0300

    try opening ODS

---
+++ src/main/java/br/com/pontualimportbrindes/staging/OpenODS.java
@@ -0,0 +1,9 @@
+package br.com.pontualimportbrindes.staging;
+
+import static br.com.pontualimportbrindes.util.Convenience.print;
+
+public class OpenODS {
+    public static void main(String[] args) {
+        print("Open ODS and populate Product instances");
+    }
+}
